b4b05b4fbff8f04d32c1f4149bec24c45c9285e0
Ruchin Baliyan <ruchin.baliyan@tesco.com>
2020-11-25 22:55:42 +0530

    stack using TDD

--- src/implement/stack/Test.java
+++ src/implement/stack/Test.java
@@ -1,4 +1,17 @@
 package implement.stack;
 
-public class Test {
+public class Test
+{
+    public static void main( String[] args )
+    {
+
+        ImplStack st = new ImplStack();
+        st.push(1);
+        st.push(3);
+        st.push(4);
+        System.out.println(st.totalElements());
+        System.out.println(st.popElement());
+        System.out.println(st.peekElement());
+
+    }
 }
